c4fe4e8d7937d0c5c5a1db338e2b292dea61d072
Prashant Singh <prashantsingh5356.com>
2025-07-14 22:18:04 +0530

    tailwindCss

--- apps/web/postcss.config.mjs
+++ apps/web/postcss.config.mjs
@@ -1,6 +1,2 @@
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
-export default config;
+import postcssConfig from "@repo/tailwind-config/postcss";
+export default postcssConfig;
